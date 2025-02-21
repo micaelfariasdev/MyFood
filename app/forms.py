b30a0aa30a0a8998e33904ce781b13695ef86c1b
Micael Farias <micaelfarias.dev@gmail.com>
2025-02-21 10:31:49 -0300

    .

--- app/forms.py
+++ app/forms.py
@@ -10,65 +10,64 @@ from werkzeug.security import generate_password_hash
 
 
 estados = [
-        ('', ''),
-        ('AC', 'Acre'),
-        ('AL', 'Alagoas'),
-        ('AP', 'Amapá'),
-        ('AM', 'Amazonas'),
-        ('BA', 'Bahia'),
-        ('CE', 'Ceará'),
-        ('DF', 'Distrito Federal'),
-        ('ES', 'Espírito Santo'),
-        ('GO', 'Goiás'),
-        ('MA', 'Maranhão'),
-        ('MT', 'Mato Grosso'),
-        ('MS', 'Mato Grosso do Sul'),
-        ('MG', 'Minas Gerais'),
-        ('PA', 'Pará'),
-        ('PB', 'Paraíba'),
-        ('PR', 'Paraná'),
-        ('PE', 'Pernambuco'),
-        ('PI', 'Piauí'),
-        ('RJ', 'Rio de Janeiro'),
-        ('RN', 'Rio Grande do Norte'),
-        ('RS', 'Rio Grande do Sul'),
-        ('RO', 'Rondônia'),
-        ('RR', 'Roraima'),
-        ('SC', 'Santa Catarina'),
-        ('SP', 'São Paulo'),
-        ('SE', 'Sergipe'),
-        ('TO', 'Tocantins')
-    ]
+    ('', ''),
+    ('AC', 'Acre'),
+    ('AL', 'Alagoas'),
+    ('AP', 'Amapá'),
+    ('AM', 'Amazonas'),
+    ('BA', 'Bahia'),
+    ('CE', 'Ceará'),
+    ('DF', 'Distrito Federal'),
+    ('ES', 'Espírito Santo'),
+    ('GO', 'Goiás'),
+    ('MA', 'Maranhão'),
+    ('MT', 'Mato Grosso'),
+    ('MS', 'Mato Grosso do Sul'),
+    ('MG', 'Minas Gerais'),
+    ('PA', 'Pará'),
+    ('PB', 'Paraíba'),
+    ('PR', 'Paraná'),
+    ('PE', 'Pernambuco'),
+    ('PI', 'Piauí'),
+    ('RJ', 'Rio de Janeiro'),
+    ('RN', 'Rio Grande do Norte'),
+    ('RS', 'Rio Grande do Sul'),
+    ('RO', 'Rondônia'),
+    ('RR', 'Roraima'),
+    ('SC', 'Santa Catarina'),
+    ('SP', 'São Paulo'),
+    ('SE', 'Sergipe'),
+    ('TO', 'Tocantins')
+]
+
 
 class ClienteForm(FlaskForm):
-    nome = StringField('Nome' , validators=[DataRequired()])
+    nome = StringField('Nome', validators=[DataRequired()])
     email = EmailField('E-Mail', validators=[DataRequired()])
     telefone = TelField('Telefone', validators=[DataRequired(), Length(4, 16)])
     end_rua = StringField('Endereço', validators=[DataRequired()])
     end_numero = StringField('Número', validators=[DataRequired()])
     end_bairro = StringField('Bairrro', validators=[DataRequired()])
     end_cidade = StringField('Cidade', validators=[DataRequired()])
-    end_estado = SelectField('Estado', choices=estados, validators=[DataRequired()])
+    end_estado = SelectField('Estado', choices=estados,
+                             validators=[DataRequired()])
     submit = SubmitField('Cadastrar')
-    
-
-    
 
     def save(self, empresa_id):
-        endereco={
-        'Rua' : self.end_rua.data,
-        'Numero' : self.end_numero.data,
-        'Bairro' : self.end_bairro.data,
-        'Cidade' : self.end_cidade.data,
-        'Estado': self.end_estado.data,
-    }
-        
+        endereco = {
+            'Rua': self.end_rua.data,
+            'Numero': self.end_numero.data,
+            'Bairro': self.end_bairro.data,
+            'Cidade': self.end_cidade.data,
+            'Estado': self.end_estado.data,
+        }
+
         cliente = Cliente(
-            nome = self.nome.data,
-            telefone = self.telefone.data,
-            email = self.email.data,
-            endereco = dumps(endereco),
-            empresa_id = empresa_id
+            nome=self.nome.data,
+            telefone=self.telefone.data,
+            email=self.email.data,
+            endereco=dumps(endereco),
+            empresa_id=empresa_id
         )
 
         db.session.add(cliente)
@@ -76,11 +75,13 @@ class ClienteForm(FlaskForm):
 
         flash('Cliente cadastrada com sucesso!', 'success')
 
+
 class ProdutoForm(FlaskForm):
     nome = StringField('')
     valor = FloatField('')
     quantidade = IntegerField('')
     valort = FloatField('')
+
 
 class PedidoForm(FlaskForm):
     codigo = StringField('Código')
@@ -91,11 +92,9 @@ class PedidoForm(FlaskForm):
     produtos = TextAreaField('Produtos', validators=[DataRequired()])
     submit = SubmitField('Cadastrar Pedido')
 
-    
     def save(self):
         from json import loads  # Importar para converter JSON de string para lista de dicionários
         from app import db, Pedido  # Evitar erro de importação circular
-
 
         # Converter produtos de JSON (string) para lista de dicionários
         produtos_json = loads(self.produtos.data)
@@ -117,37 +116,44 @@ class PedidoForm(FlaskForm):
         flash('Pedido feito com sucesso!', 'success')
         return novo_pedido
 
+
 class EmpresaForm(FlaskForm):
     # Campos do formulário da empresa
-    user = StringField('Usuário', validators=[DataRequired(), Length(min=4, max=50)])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(min=6, max=20)])
-    confirmsenha = PasswordField('Confirme sua senha', validators=[DataRequired(), EqualTo('senha'),Length(min=6, max=20)])
+    user = StringField('Usuário', validators=[
+                       DataRequired(), Length(min=4, max=50)])
+    senha = PasswordField('Senha', validators=[
+                          DataRequired(), Length(min=6, max=20)])
+    confirmsenha = PasswordField('Confirme sua senha', validators=[
+                                 DataRequired(), EqualTo('senha'), Length(min=6, max=20)])
     nome = StringField('Nome da Empresa', validators=[DataRequired()])
     razao_social = StringField('Razão Social', validators=[DataRequired()])
-    cnpj = StringField('CNPJ', validators=[DataRequired(),Length(max=18)])
+    cnpj = StringField('CNPJ', validators=[DataRequired(), Length(max=18)])
     email = EmailField('E-mail', validators=[DataRequired(), Email()])
     telefone = StringField('Telefone', validators=[DataRequired()])
     end_rua = StringField('Endereço', validators=[DataRequired()])
     end_numero = StringField('Número', validators=[DataRequired()])
     end_bairro = StringField('Bairrro', validators=[DataRequired()])
     end_cidade = StringField('Cidade', validators=[DataRequired()])
-    end_estado = SelectField('Estado', choices=estados, validators=[DataRequired()])  # Aqui você pode usar JSON ou string simples para o endereço
-    
+    # Aqui você pode usar JSON ou string simples para o endereço
+    end_estado = SelectField('Estado', choices=estados,
+                             validators=[DataRequired()])
+
     submit = SubmitField('Cadastrar Empresa')
 
     def validate_user(self, field):
         user = Empresa.query.filter_by(user=field.data).first()
         if user:
-            raise ValidationError('O nome de usuário já está em uso. Escolha outro.')
+            raise ValidationError(
+                'O nome de usuário já está em uso. Escolha outro.')
 
     def save(self):
-        endereco={
-        'Rua' : self.end_rua.data,
-        'Numero' : self.end_numero.data,
-        'Bairro' : self.end_bairro.data,
-        'Cidade' : self.end_cidade.data,
-        'Estado': self.end_estado.data,
-    }
+        endereco = {
+            'Rua': self.end_rua.data,
+            'Numero': self.end_numero.data,
+            'Bairro': self.end_bairro.data,
+            'Cidade': self.end_cidade.data,
+            'Estado': self.end_estado.data,
+        }
         senha_hash = generate_password_hash(self.senha.data)
         nova_empresa = Empresa(
             user=self.user.data,
@@ -169,17 +175,19 @@ class EmpresaForm(FlaskForm):
 
         flash('Empresa cadastrada com sucesso!', 'success')
 
+
 class LoginForm(FlaskForm):
     user = StringField('Usuario', validators=[DataRequired()])
     senha = PasswordField('Senha', validators=[DataRequired()])
     submit = SubmitField('Entrar')
 
+
 class ProdutosForm(FlaskForm):
-    nome =StringField('Nome do Produto', validators=[DataRequired()])
-    descricao=StringField('Descrição', validators=[DataRequired()])
-    valor=FloatField('Valor', validators=[DataRequired()])
-    estoque =BooleanField('Tem estoque?', validators=[Optional()])
-    quantidade=IntegerField('Quantidade em estoque', validators=[Optional()]    )
+    nome = StringField('Nome do Produto', validators=[DataRequired()])
+    descricao = StringField('Descrição', validators=[DataRequired()])
+    valor = FloatField('Valor', validators=[DataRequired()])
+    estoque = BooleanField('Tem estoque?', validators=[Optional()])
+    quantidade = IntegerField('Quantidade em estoque', validators=[Optional()])
     submit = SubmitField('Cadastrar')
 
     def save(self, empresa_id):
@@ -195,4 +203,3 @@ class ProdutosForm(FlaskForm):
         db.session.commit()
 
         flash('Produto cadastrada com sucesso!', 'success')
-
--- app/models.py
+++ app/models.py
@@ -3,13 +3,17 @@ from flask_login import UserMixin
 from datetime import datetime
 
 pedido_produto = db.Table('pedido_produto',
-    db.Column('pedido_id', db.Integer, db.ForeignKey('pedido.id'), primary_key=True),
-    db.Column('produto_id', db.Integer, db.ForeignKey('produtos.id'), primary_key=True)
-)
+                          db.Column('pedido_id', db.Integer, db.ForeignKey(
+                              'pedido.id'), primary_key=True),
+                          db.Column('produto_id', db.Integer, db.ForeignKey(
+                              'produtos.id'), primary_key=True)
+                          )
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return Empresa.query.get(user_id)
+
 
 class Empresa(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -21,10 +25,12 @@ class Empresa(db.Model, UserMixin):
     cnpj = db.Column(db.String(14), nullable=False)
     email = db.Column(db.String, nullable=False)
     telefone = db.Column(db.String, nullable=False)
-    endereco = db.Column(db.JSON, nullable=False)  # Usando JSON para armazenar o endereço como lista
+    # Usando JSON para armazenar o endereço como lista
+    endereco = db.Column(db.JSON, nullable=False)
     produtos = db.relationship('Produtos', backref='empresa', lazy=True)
     pedidos = db.relationship('Pedido', backref='empresa', lazy=True)
     clientes = db.relationship('Cliente', backref='empresa', lazy=True)
+
 
 class Cliente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -32,50 +38,67 @@ class Cliente(db.Model):
     nome = db.Column(db.String, nullable=False)
     telefone = db.Column(db.String, nullable=False)
     email = db.Column(db.String, nullable=False)
-    endereco = db.Column(db.JSON, nullable=False)  # Usando JSON para armazenar o endereço como lista
-    pedidos_historico = db.relationship('Pedido', backref='cliente', lazy=True)  # Relacionamento com a tabela Pedidos
-    empresa_id = db.Column(db.Integer, db.ForeignKey('empresa.id'), nullable=False)
+    # Usando JSON para armazenar o endereço como lista
+    endereco = db.Column(db.JSON, nullable=False)
+    # Relacionamento com a tabela Pedidos
+    pedidos_historico = db.relationship('Pedido', backref='cliente', lazy=True)
+    empresa_id = db.Column(db.Integer, db.ForeignKey(
+        'empresa.id'), nullable=False)
 
     def __repr__(self):
         return f'<Cliente {self.nome}>'
-    
+
+
 class Produtos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String, nullable=False)
     descricao = db.Column(db.String, nullable=False)
     valor = db.Column(db.Numeric(10, 2), nullable=False)
     quantidade = db.Column(db.Integer, nullable=True)
-    empresa_id = db.Column(db.Integer, db.ForeignKey('empresa.id'), nullable=False)
+    empresa_id = db.Column(db.Integer, db.ForeignKey(
+        'empresa.id'), nullable=False)
+
 
 class Pedido(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    codigo = db.Column(db.String, nullable=False, unique=True)  # Código único do pedido
+    codigo = db.Column(db.String, nullable=False,
+                       unique=True)  # Código único do pedido
     data = db.Column(db.DateTime, default=datetime.now())
     descricao = db.Column(db.String, nullable=False)
     valor = db.Column(db.Float, nullable=False)
-    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'), nullable=False)
+    cliente_id = db.Column(db.Integer, db.ForeignKey(
+        'cliente.id'), nullable=False)
     produtos = db.Column(db.JSON, nullable=False)
-    empresa_id = db.Column(db.Integer, db.ForeignKey('empresa.id'), nullable=False)
+    empresa_id = db.Column(db.Integer, db.ForeignKey(
+        'empresa.id'), nullable=False)
 
     def __repr__(self):
         return f'<Pedido {self.codigo}>'
-    
+
 
 class Carrinho(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'), nullable=False)
+    cliente_id = db.Column(db.Integer, db.ForeignKey(
+        'cliente.id'), nullable=False)
     data_criacao = db.Column(db.DateTime, default=datetime.now)
 
-    cliente = db.relationship('Cliente', backref=db.backref('carrinhos', lazy=True))
+    cliente = db.relationship(
+        'Cliente', backref=db.backref('carrinhos', lazy=True))
+
 
 class CarrinhoProduto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    carrinho_id = db.Column(db.Integer, db.ForeignKey('carrinho.id'), nullable=False)
-    produto_id = db.Column(db.Integer, db.ForeignKey('produtos.id'), nullable=False)
+    carrinho_id = db.Column(db.Integer, db.ForeignKey(
+        'carrinho.id'), nullable=False)
+    produto_id = db.Column(db.Integer, db.ForeignKey(
+        'produtos.id'), nullable=False)
     quantidade = db.Column(db.Integer, nullable=False, default=1)
 
-    carrinho = db.relationship('Carrinho', backref=db.backref('itens', cascade="all, delete-orphan"))
-    produto = db.relationship('Produtos', backref=db.backref('em_carrinhos', cascade="all, delete-orphan"))
+    carrinho = db.relationship('Carrinho', backref=db.backref(
+        'itens', cascade="all, delete-orphan"))
+    produto = db.relationship('Produtos', backref=db.backref(
+        'em_carrinhos', cascade="all, delete-orphan"))
 
     # Garantir que o mesmo produto não seja adicionado duas vezes ao carrinho
-    __table_args__ = (db.UniqueConstraint('carrinho_id', 'produto_id', name='unique_produto_carrinho'),)
+    __table_args__ = (db.UniqueConstraint(
+        'carrinho_id', 'produto_id', name='unique_produto_carrinho'),)
